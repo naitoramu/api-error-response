69c8df3ebcbe3c05415b1414a73899f9d8228288
Jakub Baran <kubabaran2000@wp.pl>
2024-04-04 23:48:12 +0200

    initial commit

---
+++ src/main/java/io/muflon/utils/problem/Problem.java
@@ -0,0 +1,33 @@
+package io.muflon.utils.problem;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.ConstraintViolation;
+import org.springframework.http.HttpStatus;
+
+import java.util.Set;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public record Problem(
+	int status,
+	String error,
+	String details,
+	Set<Violation> violations
+) {
+
+	public Problem(HttpStatus status, String error, String details, Set<Violation> violations) {
+		this(status.value(), error, details, violations);
+	}
+
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)
+	public record Violation(
+		String field,
+		String message,
+		Object rejectedValue
+	) {
+
+		static <T> Violation fromConstraintViolation(ConstraintViolation<T> violation) {
+			return new Violation(
+				violation.getPropertyPath().toString(), violation.getMessage(), violation.getInvalidValue());
+		}
+	}
+}
---
+++ src/main/java/io/muflon/utils/problem/Problems.java
@@ -0,0 +1,64 @@
+package io.muflon.utils.problem;
+
+import jakarta.validation.ConstraintViolation;
+import org.springframework.http.HttpStatus;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Problems {
+
+	public static <T> Problem validationError(Set<ConstraintViolation<T>> violations) {
+		return new Problem(
+			HttpStatus.BAD_REQUEST,
+			"Validation error",
+			"Submitted request is invalid. See violations below for more information",
+			violations.stream().map(Problem.Violation::fromConstraintViolation).collect(Collectors.toSet())
+		);
+	}
+
+	public static Problem invalidArgumentError(Set<Problem.Violation> violations) {
+		return new Problem(
+			HttpStatus.BAD_REQUEST,
+			"Validation error",
+			"Submitted request is invalid. See violations below for more information",
+			violations
+		);
+	}
+
+	public static Problem resourceNotFound(String resourceName, String resourceId) {
+		return new Problem(
+			HttpStatus.NOT_FOUND,
+			"Resource not found",
+			"The system could not find " + resourceName + " with ID = " + resourceId,
+			null
+		);
+	}
+
+	public static Problem badRequest(String details) {
+		return new Problem(
+			HttpStatus.BAD_REQUEST,
+			"Bad request",
+			details,
+			null
+		);
+	}
+
+	public static Problem pathNotFound(String path) {
+		return new Problem(
+			HttpStatus.NOT_FOUND,
+			"Not found",
+			"The system could not find path '" + path + "'",
+			null
+		);
+	}
+
+	public static Problem internalServerError() {
+		return new Problem(
+			HttpStatus.INTERNAL_SERVER_ERROR,
+			"Internal Server Error",
+			"Unexpected error occurred. Please try again in a while.",
+			null
+		);
+	}
+}
